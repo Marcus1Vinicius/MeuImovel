c0eac8a66334ea1042a4209fe4a2bc9b63bfc56b
Marcus <omarcusvinicius96@gmail.com>
2026-02-03 13:13:42 -0300

    Iniciando o projeto

---
+++ src/main/java/com/MeuImovel/MeuImovelCRM/MeuImovelCrmApplication.java
@@ -0,0 +1,13 @@
+package com.MeuImovel.MeuImovelCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuImovelCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuImovelCrmApplication.class, args);
+	}
+
+}
