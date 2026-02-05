3e340aefab0187c3ac1b2235f6b1f37675fb6900
Marcus <omarcusvinicius96@gmail.com>
2026-02-05 16:24:17 -0300

    Adicionando os atributos da classe imovel, adicionando e ajustando atributos na classe cliente e corretor

--- src/main/java/com/MeuImovel/MeuImovelCRM/model/Corretor.java
+++ src/main/java/com/MeuImovel/MeuImovelCRM/model/Corretor.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data // Cria Getters, Setters e muito mais
 @NoArgsConstructor // Cria o construtor vazio para o Hibernate
 @AllArgsConstructor // Cria o construtor completo
@@ -14,15 +16,27 @@ public class Corretor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
     @Embedded
     private Creci creciCorretor;
+
     private String fotoCorretor;
+
     @Embedded
     private Cpf cpfCorretor;
+
     @Embedded
     private Senha senhaCorretor;
+
     @Embedded
     private Email emailCorretor;
+
+    @OneToMany(mappedBy = "corretorVendedor") // Muitos clientes para um corretor // Nome da coluna que guardará o ID do cliente no banco
+    private List<Cliente> clientesCorretor;
+
+    @OneToMany(mappedBy = "corretorResponsavel") // Muitos imoveis para um corretor // Nome da coluna que guardará o ID do imovel no banco
+    private List<Imovel> imoveisCorretor;
 
 }
--- src/main/java/com/MeuImovel/MeuImovelCRM/model/Imovel.java
+++ src/main/java/com/MeuImovel/MeuImovelCRM/model/Imovel.java
@@ -1,4 +1,49 @@
 package com.MeuImovel.MeuImovelCRM.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor // Cria o construtor vazio para o Hibernate
+@AllArgsConstructor // Cria o construtor completo
+@Entity
 public class Imovel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private int valor;
+
+    @Enumerated(EnumType.STRING)
+    private StatusImovel status; //vai ser uma lista assim como o status cliente
+
+    private String fotos;
+
+    private String videos;
+
+    private String imagem360;
+
+    private boolean financiavel; //talvez uma lista, vai servir basicamente para dizer se aquele imóovel pode ser financiado
+
+    @Embedded
+    private Cep cepImovel;
+
+    private String Bairro;
+
+    private int numero;
+
+    @Embedded
+    private Construtora construtora; // relacionamento de um para um
+
+    @ManyToOne // Muitos imoveis para um corretor
+    @JoinColumn(name = "corretor_id") // Nome da coluna que guardará o ID do corretor no banco
+    private Corretor corretorResponsavel;
+
+    @ManyToOne // Muitos imoveis para um cliente
+    @JoinColumn(name = "cliente_id") // Nome da coluna que guardará o ID do corretor no banco
+    private Cliente clienteComprador;
+
 }
