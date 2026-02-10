d9306b6993f8b2ae13fa7e19778d65cff2794170
Marcus <omarcusvinicius96@gmail.com>
2026-02-09 22:36:23 -0300

    começando o validador de celular

--- src/main/java/com/MeuImovel/MeuImovelCRM/model/Celular.java
+++ src/main/java/com/MeuImovel/MeuImovelCRM/model/Celular.java
@@ -1,6 +1,38 @@
 package com.MeuImovel.MeuImovelCRM.model;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Celular {
+    @Column(name = "celular")
+    private String celularDigitos;
+
+    protected Celular(){
+
+    }
+
+    public Celular(String celularEntrada){
+
+        boolean celularProcessado = validarDigitosCelular(celularEntrada);
+
+        if (celularProcessado){
+            throw new IllegalArgumentException("Celular invalido!");
+        }
+        this.celularDigitos = celularEntrada.replaceAll("\\D", "");
+
+    }
+
+    private boolean validarDigitosCelular(String celularDigitos){ // esse metodo vai validar o celular
+
+        if(celularDigitos == null){
+            throw new IllegalArgumentException("O celular não pode ser nulo!");
+        }
+
+        String celularLimpo = celularDigitos.replaceAll("\\D", "");
+
+        return celularLimpo.matches("^(?:1[1-9]|[2-9][1-9])9\\d{8}$"); // esse regex faz toda a validação dos celulares do brasil
+
+    }
 }
+
+
