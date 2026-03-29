6159ab7a5c6f92f3ee3d97d649a67ca68e2ed19a
Marcus <omarcusvinicius96@gmail.com>
2026-03-29 10:58:42 -0300

    ajuste de segurança na URL da api viacep

--- src/main/java/com/MeuImovel/MeuImovelCRM/service/ViaCepService.java
+++ src/main/java/com/MeuImovel/MeuImovelCRM/service/ViaCepService.java
@@ -2,6 +2,8 @@ package com.MeuImovel.MeuImovelCRM.service;
 
 import com.MeuImovel.MeuImovelCRM.dto.ViaCepResponse;
 import com.MeuImovel.MeuImovelCRM.model.Cep;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
 import java.net.URI;
@@ -9,13 +11,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+@Service
 public class ViaCepService {
+    @Value("${viacep.api.url}")
+    private String urlViaCep;
     public ViaCepResponse getCep(String cep){
         ViaCepResponse enderecoDto = null;
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://viacep.com.br/ws/"+ cep +"/json/")).build();
+                    .uri(URI.create(urlViaCep+ cep +"/json/")).build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             ObjectMapper mapper = new ObjectMapper();
